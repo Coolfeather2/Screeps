9d3d6cdcb4c8d8769f935176f3e36a41f85bb721
Coolfeather2 <coolfeather.o@gmail.com>
2017-09-11 04:25:08 +0800

    Some Optimizations

--- main.js
+++ main.js
@@ -62,22 +62,23 @@ module.exports.loop = function () {
         }
     }
 
-    var minimumNumberOfHarvesters = 1;
-    var minimumNumberOfUpgraders = 1;
+    var minimumNumberOfHarvesters = 2;
+    var minimumNumberOfUpgraders = 2;
     var minimumNumberOfBuilders = 2;
     var minimumNumberOfRepairers = 1;
     var minimumNumberOfWallRepairers = 1;
-    var minimumNumberOfLongDistanceHarvesters = 2;
+    var minimumNumberOfLongDistanceHarvesters = 0;
     
-    var numberOfHarvesters = _.sum(Game.creeps, (c) => c.memory.role == 'Harvester');
-    var numberOfUpgraders = _.sum(Game.creeps, (c) => c.memory.role == 'Upgrader');
-    var numberOfBuilders = _.sum(Game.creeps, (c) => c.memory.role == 'Builder');
-    var numberOfRepairers = _.sum(Game.creeps, (c) => c.memory.role == 'Repairer');
-    var numberOfWallRepairers = _.sum(Game.creeps, (c) => c.memory.role == 'WallRepairer');
-    var numberOfLongDistanceHarvesters = _.sum(Game.creeps, (c) => c.memory.role == 'LongDistanceHarvester');
+    let creepsByRole = _.countBy(Game.creeps, 'memory.role');
+    let numberOfHarvesters = creepsByRole['Harvester'] || 0; 
+    let numberOfUpgraders = creepsByRole['Upgrader'] || 0; 
+    let numberOfBuilders = creepsByRole['Builder'] || 0; 
+    let numberOfRepairers = creepsByRole['Repairer'] || 0; 
+    let numberOfWallRepairers = creepsByRole['WallRepairer'] || 0; 
+    let numberOfLongDistanceHarvesters = creepsByRole['LongDistanceHarvester'] || 0;
     
     var energy = Game.spawns.Spawn1.room.energyCapacityAvailable;
-    var name = undefined
+    var name;
 
     if (numberOfHarvesters < minimumNumberOfHarvesters) {
         // try to spawn one
@@ -89,12 +90,12 @@ module.exports.loop = function () {
             name = Game.spawns.Spawn1.createCustomCreep(Game.spawns.Spawn1.room.energyAvailable, 'Harvester');
         }
     }
-    else if (Game.spawns.Spawn1.memory.claimRoom != undefined) {
+/**    else if (Game.spawns.Spawn1.memory.claimRoom != undefined) {
         name = Game.spawns.Spawn1.createClaimer(Game.spawns.Spawn1.memory.claimRoom);
         if (!(name < 0)) {
             delete Game.spawns.Spawn1.memory.claimRoom;
         }
-    }
+    }**/
     // if not enough upgraders
     else if (numberOfUpgraders < minimumNumberOfUpgraders) {
         // try to spawn one
@@ -121,17 +122,25 @@ module.exports.loop = function () {
     else {
     }
 
-//    if (!(name < 0)) {
-//        console.log('----------------------------------------');
-//        console.log("spawned new creep: " + name + " (" + Game.creeps[name].memory.role + ")");
-//        console.log("Harvesters    : " + numberOfHarvesters);
-//        console.log("Upgraders     : " + numberOfUpgraders);
-//        console.log("Builders      : " + numberOfBuilders);
-//        console.log("Repairers     : " + numberOfRepairers);
-//        console.log("WallRepairers : " + numberOfWallRepairers);
-//        console.log("Long Harvester: " + numberOfLongDistanceHarvesters);
-//        console.log('----------------------------------------');
-    //    }
+    if (name && typeof name === 'string') {   
+        let numberOfHarvesters = creepsByRole['Harvester'] || 0;
+        let numberOfUpgraders = creepsByRole['Upgrader'] || 0;
+        let numberOfBuilders = creepsByRole['Builder'] || 0;
+        let numberOfRepairers = creepsByRole['Repairer'] || 0;
+        let numberOfWallRepairers = creepsByRole['WallRepairer'] || 0;
+        let numberOfLongDistanceHarvesters = creepsByRole['LongDistanceHarvester'] || 0;
+
+        console.log('----------------------------------------');
+        console.log("Spawned new creep: " + name + " (" + Game.creeps[name].memory.role + ")");
+        console.log("Harvesters    : " + numberOfHarvesters + "/" + minimumNumberOfHarvesters);
+        console.log("Upgraders     : " + numberOfUpgraders + "/" + minimumNumberOfUpgraders);
+        console.log("Builders      : " + numberOfBuilders + "/" + minimumNumberOfBuilders);
+        console.log("Repairers     : " + numberOfRepairers + "/" + minimumNumberOfHarvesters);
+        console.log("WallRepairers : " + numberOfWallRepairers + "/" + minimumNumberOfRepairers);
+        console.log("Long Harvester: " + numberOfLongDistanceHarvesters + "/" + minimumNumberOfLongDistanceHarvesters);
+        console.log('----------------------------------------');
+    }
+
     screepsplus.collect_stats()
     Memory.stats.cpu.used = Game.cpu.getUsed();
 };
